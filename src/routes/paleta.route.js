7ebfcec5d749fc11e6fbc0c49c7c78a2f9923bdd
lauraol <lauraoliveirape@gmail.com>
2022-08-04 17:01:49 -0300

    adding more comments about mvc

--- src/routes/paleta.route.js
+++ src/routes/paleta.route.js
@@ -20,4 +20,6 @@ module.exports = router; // exportando o arquivo paleta.route.js que contém tod
   mais um cometário importante (vou colocar no route, controller e service):
   fluxo:
   acessamos a nossa rota em alguma função do controller > controller chama o service > service manda o que foi solicitado para a controler > controler exibe o que a função pede.
+  simplificando mais ainda pois é sempre bom:
+  o index executa as rotas > as rotas executam o controller > o controller irá solicitar os dados para o service, tratar esse dados e retornar para o cliente passando essas dados por meio das rotas que são executadas lá no index (onde está o nosso servidor).
 */
